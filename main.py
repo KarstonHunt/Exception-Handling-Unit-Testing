fbd84bba72938378e04865067d45fd86a5620c3a
KarstonHunt <KarstonHunt@protonmail.com>
2023-01-17 12:03:59 -0800

    Defined name_the_number function. Created try block with exception for OutOfRangeError

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+class OutOfRangeError(Exception):
+    """Exception for number out of range"""
+    pass
+
+
 def main():
-    class OutOfRangeError(Exception):
-        """Exception for number out of range"""
-        pass
+
+    def name_the_number():
+        user_number = int(input("Please enter an integer: "))
+        if user_number == 1:
+            print("one")
+        elif user_number == 2:
+            print("two")
+        elif user_number == 3:
+            print("three")
+        else:
+            raise OutOfRangeError
+
+    try:
+        name_the_number()
+    except OutOfRangeError:
+        print("That's not one of the allowed values!")
+
 
 if __name__ == '__main__':
     main()
+
